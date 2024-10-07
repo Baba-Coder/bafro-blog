1023dd079b421b19f588f78b951adbf8fd11051e
Baba-Coder <bolaniyi.mvc@gmail.com>
2024-10-07 03:42:39 -0500

    Little changes rrenamed SignIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Signin from './pages/Signin';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from '.components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
        <Route path="/" element={<Home />} />
        <Route path="/about" element={<About />} />
-       <Route path="/sign-in" element={<Signin />} />
+       <Route path="/sign-in" element={<SignIn />} />
        <Route path="/sign-up" element={<SignUp />} />
        <Route path="/dashboard" element={<Dashboard />} />
        <Route path="/projects" element={<Projects />} />
